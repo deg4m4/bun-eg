4ad2c16952c0b02e1fc52afcf5327800f6cc0ef2
Parthka <73784979+meparthka@users.noreply.github.com>
2022-07-13 00:45:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 
+const port = Bun.argv[2] || "4545";
+
+Bun.serve({
+
+    async fetch(req) {
+
+        await req.text();
+
+        return new Response("Hello World");
+
+    },
+
+    port: Number(port),
+
+});
